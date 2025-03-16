e8a8241d0c11e21e1553484b4d6fa6bbedb9edcb
Zahin_Abdullah <zahinabdullahrakin@gmail.com>
2025-03-16 12:24:07 +0600

    learned to get variable from the .env file

--- backend/backend/app.py
+++ backend/backend/app.py
@@ -1,11 +1,9 @@
-from fastapi import FastAPI # type: ignore
+from fastapi import FastAPI # type:ignore
 import uvicorn #type: ignore
-from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware # type: ignore
+from fastapi.middleware.cors import CORSMiddleware #type: ignore
+
 
 app = FastAPI()
-
-
 origins = [
   "http://localhost:5173",
   "http://localhost:5174",
@@ -17,12 +15,11 @@ app.add_middleware(
   allow_origins=origins,
   allow_credentials=True,
   allow_methods=["*"],
-  allow_headers=["*"],
+  allow_headers=["*"]
 )
 
 @app.get("/api/v1/healthcheck")
 async def healthcheck():
   return {
-    "name": "zahin",
-    "age" : "21"
+    "working" : True
   }
--- backend/backend/main.py
+++ backend/backend/main.py
@@ -1,7 +1,18 @@
-import uvicorn  # type: ignore
+import uvicorn #type: ignore
+from pydantic_settings import BaseSettings # type: ignore
+
+class Settings(BaseSettings):
+  AUTHOR: str
+  
+  class Config:
+    env_file = ".env"
+
 
 def main():
-    uvicorn.run("backend.app:app", host="localhost", port=8000, reload=True)
+  settings = Settings()
+  print(settings.AUTHOR) # debugging log
+  uvicorn.run("backend.app:app", host="localhost", port=8000, reload=True)
+
 
 if __name__ == "__main__":
-    main()
+  main()
